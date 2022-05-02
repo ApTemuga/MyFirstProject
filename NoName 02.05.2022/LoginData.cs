5df3687ab3abd1322a2b9a1f9abe397d60d099ed
agardat2@yandex.ru <igor.bobrov.aleksandrovich@gmail.com>
2022-05-02 17:14:00 +0700

    Auth window prototipe

---
+++ NoName 02.05.2022/LoginData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoName_02._05._2022
+{
+    class LoginData
+    {
+        
+    }
+}
